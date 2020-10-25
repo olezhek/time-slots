9af6e0df5ddb3af85fc33d45b424d762b6cb59c2
olzk <mail@olezhek.net>
2020-10-25 21:37:56 +0100

    timeslot layout

--- src/components/calendar/TimeSlot.js
+++ src/components/calendar/TimeSlot.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import classnames from 'classnames'
+import { formatTime } from '../../utils'
 
 export default function TimeSlot({ from, to, disabled, active, onSlotClick }) {
   return (
     <button
       onClick={() => { onSlotClick({ start_time: from, end_time: to }) }}
       key={`${from}-${to}`}
-      className={classnames('list-group-item', { active })}>
-      <div className={classnames("d-flex align-items-center justify-content-between", { disabled })}>
-        <div className="d-flex flex-column">
-          <div><span>From:</span> {from}</div>
-          <div><span>To:</span> {to}</div>
-        </div>
-      </div>
-    </button>
+      className={classnames('list-group-item', { active })}
+      dangerouslySetInnerHTML={{ __html: `${formatTime(from)} &ndash; ${formatTime(to)}` }}
+    />
   )
 }
