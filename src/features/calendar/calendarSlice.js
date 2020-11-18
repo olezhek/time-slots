574ebb26464ebfb1d42888a3ebe567949d0c7860
olzk <mail@olezhek.net>
2020-11-18 22:24:27 +0100

    todo

--- src/features/calendar/calendarSlice.js
+++ src/features/calendar/calendarSlice.js
@@ -17,6 +17,7 @@ export const slice = createSlice({
   reducers: {
     setData: (state, action) => {
       state.data = (action.payload || []).map(({ time_slots, ...companyInfo }) => {
+        // TODO: fix typo in or
         const timeSlots = ([...time_slots] || []).reduce((slots, slot) => {
           const date = moment(slot.start_time).format('YYYY-MM-DD')
           if (!slots[date]) {
